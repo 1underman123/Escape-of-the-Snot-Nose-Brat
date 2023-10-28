81118970b68e30b23bed602c626d02eaf1173cf8
1underman123 <gjw4492@gmail.com>
2023-10-28 17:33:13 -0400

    first commit

---
+++ classes.py
@@ -0,0 +1,89 @@
+import pygame
+from pygame.sprite import Sprite
+
+import pygame
+from pygame.sprite import Sprite
+
+class Player(Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+
+        self.image = pygame.image.load('graphics/brat/brat_idle.png').convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.touching_ground = False
+        self.looking_left = False
+        self.velocity = pygame.Vector2(0, 0)  # Create a Vector2 object
+
+        self.jump_strength = -3.5  # Adjust jump strength as needed
+        self.gravity = 0.25  # Adjust gravity as needed
+
+    def update(self, keys, platforms):
+        self.velocity.x = 0  # Reset horizontal velocity
+
+        # Adjust horizontal velocity based on key presses
+        if keys[pygame.K_LEFT]:
+            self.velocity.x = -0.9
+        elif keys[pygame.K_RIGHT]:
+            self.velocity.x = 0.9
+
+        # Apply gravity
+        self.velocity.y += self.gravity
+
+        # Limit maximum falling speed
+        if self.velocity.y > 10:
+            self.velocity.y = 10
+
+        self.move_with_collision(platforms)  # Handle collisions and move
+
+        if keys[pygame.K_SPACE] and self.touching_ground == True:
+            self.jump()
+            self.touching_ground = False
+            
+
+    def move_with_collision(self, platforms):
+        self.rect.x += self.velocity.x
+
+        collisions = pygame.sprite.spritecollide(self, platforms, False)
+        for platform in collisions:
+            if self.velocity.x > 0:
+                self.rect.right = platform.rect.left
+            elif self.velocity.x < 0:
+                self.rect.left = platform.rect.right
+
+        self.rect.y += self.velocity.y
+
+        collisions = pygame.sprite.spritecollide(self, platforms, False)
+        for platform in collisions:
+            if self.velocity.y > 0:
+                self.rect.bottom = platform.rect.top
+                self.velocity.y = 0
+                self.touching_ground = True
+            elif self.velocity.y < 0:
+                self.rect.top = platform.rect.bottom
+                self.velocity.y = 0
+
+    def jump(self):
+        self.velocity.y = self.jump_strength
+
+    def draw(self, screen):
+        if self.looking_left == True:
+            screen.blit(pygame.transform.flip(self.image, True, False), self.rect)
+        else:
+            screen.blit(self.image, self.rect)
+        
+
+
+class Platform(Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface((width, height))
+        self.image.fill((0, 255, 0))  # Green color for platforms
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame
+from classes import Player, Platform
+from sys import exit
+
+pygame.init()
+flags = pygame.SCALED | pygame.RESIZABLE
+screen = pygame.display.set_mode((80,60),flags)
+pygame.display.set_caption('game')
+clock = pygame.time.Clock()
+test_font = pygame.font.Font(None, 50)
+gameRun = True
+
+# create player & put it in GroupSingle
+brat = pygame.sprite.GroupSingle()
+brat.add(Player(10,10))
+# make platforms & put in a group
+platform1 = Platform(-1000, 55, 2000, 20)
+platforms_group = pygame.sprite.Group()
+platforms_group.add(platform1)
+
+
+while gameRun:
+    # event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    brat.update(pygame.key.get_pressed(), platforms_group)
+    
+    # draw things idk
+    screen.fill('White')
+    platforms_group.draw(screen)
+    brat.draw(screen)
+
+    # update the screen
+    pygame.display.update()
+    pygame.display.flip()
+    
+    # cap at 60 fps
+    clock.tick(60)
